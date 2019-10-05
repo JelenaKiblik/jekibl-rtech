fa49390862dbfeb3052d2a2b85bd5027da3c4868
JelenaKiblik <jekibl@ttu.ee>
2019-10-05 12:10:05 +0300

    Third homework done

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/jekibl-rtech/src
+# Build directory: /home/ubuntu/jekibl-rtech/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
+subdirs(my_r2d2)
+subdirs(r2d2_navigator)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/jekibl-rtech/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/ubuntu/jekibl-rtech/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/jekibl-rtech/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ubuntu/jekibl-rtech/src:$ROS_PACKAGE_PATH"
