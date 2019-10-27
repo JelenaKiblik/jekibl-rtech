6c618aaad61c7782ad2fba8890ca6a3c684fe001
JelenaKiblik <jekibl@ttu.ee>
2019-10-27 19:19:27 +0200

    1 part of 7 homework done

---
+++ devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(turtlebot3_msgs_MESSAGE_FILES "/home/ubuntu/jekibl-rtech/src/turtlebot3_msgs/msg/SensorState.msg;/home/ubuntu/jekibl-rtech/src/turtlebot3_msgs/msg/VersionInfo.msg;/home/ubuntu/jekibl-rtech/src/turtlebot3_msgs/msg/Sound.msg")
+set(turtlebot3_msgs_SERVICE_FILES "")
